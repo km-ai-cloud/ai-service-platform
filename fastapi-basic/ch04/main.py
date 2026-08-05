aff5e8a6d26568398690b59ed28811ae3497f2f9
lee <kyumi.lecture@gmail.com>
2026-08-05 17:53:18 +0900

    2026-08-05 commit

--- fastapi-basic/ch04/main.py
+++ fastapi-basic/ch04/main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI
-from routes.todo import todo_router
-# from routes.book import book_router
+#from routes.todo import todo_router
+from routes.book import book_router
 from database import Base, engine
 
 # todos 테이블 생성
@@ -8,5 +8,5 @@ Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 
-app.include_router(todo_router) # todo 애플리케이션
-# app.include_router(book_router) # 도서관리 애플리케이션
+#app.include_router(todo_router) # todo 애플리케이션
+app.include_router(book_router) # 도서관리 애플리케이션
